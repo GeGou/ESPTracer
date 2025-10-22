b1d3e76179a3cbbd88105d5bdb812c67c10c52a7
gegou <geogoumas95@gmail.com>
2025-10-22 08:20:13 +0300

    Battery read check

--- src/mqttManager.cpp
+++ src/mqttManager.cpp
@@ -92,6 +92,7 @@ float ReadBatteryVoltage() {
 
     uint32_t raw_mv = analogReadMilliVolts(BOARD_BAT_ADC_PIN);  // mV
     float voltage = (raw_mv / 1000.0) * VOLTAGE_DIVIDER; // convert to volts
+    // float voltage *= VOLTAGE_DIVIDER; // convert to volts
 
     // sanity check
     if (voltage < 2.5 || voltage > 5.0) return 0;
